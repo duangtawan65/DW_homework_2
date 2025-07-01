a72f8f7caa7004dcc64142bfff2bcb35836c8a51
duangtawan65 <duangtawan.si.65@ubu.ac.th>
2025-07-01 16:13:45 +0700

    wh2

--- data_wh/data_wh/settings.py
+++ data_wh/data_wh/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -70,7 +71,7 @@ TEMPLATES = [
 ]
 
 ASGI_APPLICATION = "data_wh.asgi.application"
-WSGI_APPLICATION = "data_wh.wsgi.application"
+# WSGI_APPLICATION = "data_wh.wsgi.application"
 
 
 # Database
